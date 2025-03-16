6e5507af5822019cf7218292e85f829e37bf7be9
Eryk <eryktkaczewski@gmail.com>
2025-03-16 22:08:12 +0100

    added Team and TeamTest

--- src/main/java/org/example/Team.java
+++ src/main/java/org/example/Team.java
@@ -1,11 +1,29 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Team {
     private final String teamName;
 
 
     public Team(String teamName) {
+        if (teamName == null ||  teamName.isBlank()) {
+            throw new IllegalArgumentException("Team name cannot be null or empty");
+        }
         this.teamName = teamName;
     }
 
+    public String getTeamName() {return teamName;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return Objects.equals(teamName, team.teamName);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamName);
+    }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -7,11 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TeamTest {
 
     @Test
-    void shouldCreateTeamWithName() {}
+    void shouldCreateTeamWithName() {
+        Team team = new Team("Brazil");
+        assertEquals("Brazil", team.getTeamName());
+    }
 
     @Test
-    void shouldReturnErrorForNullOrEmptyName() {}
+    void shouldReturnErrorForNullOrEmptyName() {
+        assertThrows(IllegalArgumentException.class, () -> new Team(null));
+        assertThrows(IllegalArgumentException.class, () -> new Team(""));
+        assertThrows(IllegalArgumentException.class, () -> new Team("   "));
+    }
 
     @Test
-    void teamWithTheSameNameShouldBeEqual() {}
+    void teamWithTheSameNameShouldBeEqual() {
+        Team team1 = new Team("Brazil");
+        Team team2 = new Team("Brazil");
+        Team team3 = new Team("Germany");
+
+        assertEquals(team1, team2);
+        assertNotEquals(team1, team3);
+        assertEquals(team1.hashCode(), team2.hashCode());
+    }
 }
